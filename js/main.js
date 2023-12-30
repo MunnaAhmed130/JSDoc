ef11672c58a808322f0fb1ff931e1ed97f6d1649
Munna Ahmed <munnaahmed2025@gmail.com>
2023-12-30 12:39:49 +0600

    commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const str = "hey world";
+str = "hey world";
+str = "hey world";
+str = "hey world";
+
+function add() {
+    const result = num1 + num2;
+    return result;
+}
+
+add(1, 2);
+
+console.log(str);
+
+// This is a comment
+
+let word = "hello world";
+str = "hello world";
+str = "hello world";
+str = "hello world";
+
+function add(num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+
+add(1, 2);
+
+console.log(str);
+
+console.log(str);
+console.log(str);
---
+++ text.js
@@ -0,0 +1,51 @@
+
+asdfghlk asdfsdfghlk asdfghlkjjjjj
+
+Hey 
+there!
+
+ks
+asdfghlkl asdfghlklll asdfghlkll asdfghlkll asdfghlkll asdfghlkl     l
+asdfghlkRRRRRuuhlkj asdfghlkj asdfghlk asdfghlk asdfghlkjjjasdfghlk asdfghlkjjjjasdfghlk asdfghlkjjjjasdfghlk asdfghlkjjjjjasdfghlk asdfghlkjjjjjasdfghlk asdfghlkjjjjjasdfghlk asdfghlkjjjjjjj
+hey what's up
+asdfghlkjjasdfghlkjj
+asdfghlkjjasdfghlkjj
+hey what's up
+there!
+Hello there!djkHello there!djkHello there!djkHello there!djkHello there!djkHello there!djk
+Hello there!djkHello there!djkHello there!djkHello there!djkHello there!djk
+
+akkasdfghlkjjsdfghlkjj
+hey what's up
+asdfghlkjjasdfghlkjj
+function hello (para, param){
+	const num = para;
+	return num
+}
+Hello there!djk
+asdfghlkjjasdfghlkjj
+asdfghlkjjasdfghlkjj
+asdfghlkjjasdfghlkjj
+asdfghlkjjasdfghlkjj
+"Hello there!djk"
+asdfghlkjjasdfghlkjj
+asdfghlkjjasdfghlkjj
+asdfghlkjjasdfghlkjj
+asdfghlkjjasdfghlkjj
+asdfghlkjjasdfghlkjj
+asdfghlkjjasdfghlkjj
+asdfghlkjjasdfghlkjj
+asdfghla 
+asdfghlkjjasdfghlkjj
+asdfghlkjjasdfghlkjj
+asdfghlkjjl
+aasasdfghlkjjdfghlkjjsdfghlkjj
+asdfghlkjjasdfghlkjjasdfghlkjjasdfghlkjj
+asdfghlkjasdfghlkjjjjjjjj
+
+
+asdfghbbb asdfghlkjjjjjasdfghlk asdfghlkjjjjj
+asdfghlk asdfghlkjjjjj
+asdfghlk asdfghiiilllkjjjjj
+bsdfghlk asdfghlkjjjjjg
+

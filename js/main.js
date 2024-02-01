6f41bb5c51c6c4262d169111d05616730abef37c
Munna Ahmed <munnaahmed2025@gmail.com>
2024-02-02 00:15:20 +0600

    commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 let text = "hey world";
-
 // cursor on 1st letter f<last letter> ~
 // change case
 text = "hey world";
 text = "HEY WORLD";
 text = "hey world";
-// add function
-function add() {
+
+/** This function adds 2 numbers and returns a number
+ * @param {number} num1 - any number
+ * @param {number} num2 - any number
+ * @returns {number} - the sum of 2 numbers
+ */
+function add(num1, num2) {
     const result = num1 + num2;
     return result;
 }
@@ -15,16 +19,9 @@ add(1, 2);
 console.log(text);
 
 // This is a comment
-let word = "hello world";
 text = "hello world";
 text = "hello world";
 text = "hello world";
-
-function add(num1, num2) {
-    const result = num1 + num2;
-    return result;
-}
-add(1, 2);
 
 console.log(text);
 

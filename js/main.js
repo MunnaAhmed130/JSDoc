ae3eae1bdbf35ae2f0144223bdb1ee1c91b85419
Munna Ahmed <munnaahmed2025@gmail.com>
2024-02-03 02:41:51 +0600

    commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 /** a random text
  * @type {string}
  */
-const text = "Hello World";
+let text = "Hello World";
 
 /** Array of grades
  * @type {Array<number>}
@@ -18,7 +18,7 @@ const user = {
     name: "adam",
 };
 
-/** This function adds 2 numbers and returns a number
+/** adds 2 numbers
  * @param {number} num1 - any number
  * @param {number} num2 - any number
  * @returns {number} - the sum of 2 numbers
@@ -34,3 +34,34 @@ console.log(text);
 // console.log(text);
 // console.log(text);
 // console.log(text);
+
+/** calaulate tax amount
+ * @param {number} amount - Total amount
+ * @param {number} tax  - Tax percentage
+ * @returns {string} - Total with a dollar($) sign
+ */
+function calculateTax(amount, tax) {
+    return `$${amount + tax * amount}`;
+}
+
+let value = calculateTax(2000, 4);
+
+// custom type/ type def/definition
+/**
+ * A student
+ * @typedef {object} Student
+ * @property {number} id - student id
+ * @property {string} name - student name
+ * @property {string|number} [age] - student age (optional)
+ * @property {boolean} isActive - student is active
+ */
+
+/**
+ * @type {Student}
+ */
+const student = {
+    id: 1,
+    name: "John Doe",
+    age: 20,
+    isActive: true,
+};

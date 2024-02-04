e428a651a54f086b5302781449affd7ad9fc28b5
Munna Ahmed <munnaahmed2025@gmail.com>
2024-02-05 01:36:46 +0600

    commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 // @ts-check
+
+/**
+ * @file index.js is the root file for this example
+ * @author Munna Ahmed
+ * @see {@link https://github.com/MunnaAhmed130|Github}
+ */
+
 const { add, divide, subtract, multiply } = require("./calculator");
 
 console.log(add(3, 3));
